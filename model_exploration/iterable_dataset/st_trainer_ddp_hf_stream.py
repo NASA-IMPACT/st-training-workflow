661d7c2d9daf616762c7b430d6c797ef60b7c34d
Sajil Awale <sa0812@uah.edu>
2025-07-02 21:15:19 -0500

    .

--- model_exploration/iterable_dataset/st_trainer_ddp_hf_stream.py
+++ model_exploration/iterable_dataset/st_trainer_ddp_hf_stream.py
@@ -69,7 +69,7 @@ parser.add_argument("--num_train_epochs", type=int, default=1)
 parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--warmup_ratio", type=float, default=0.1)
 parser.add_argument("--eval_and_save_steps", type=int, default=1000)
-parser.add_argument("--max_datapoints_per_src_for_eval", type=int, default=20)
+parser.add_argument("--max_datapoints_per_src_for_eval", type=int, default=-1)
 parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
 parser.add_argument("--lr", type=float, default=1e-5)
 parser.add_argument(
