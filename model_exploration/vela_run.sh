a07c6b0f2e1c5f1f3c71b4acd12aa19290a9d4d5
Vishnudev K <vishnudev.k@ibm.com>
2025-09-17 09:34:47 +0000

    script to run jobs on vela cluster

---
+++ model_exploration/vela_run.sh
@@ -0,0 +1,11 @@
+torchrun --nproc_per_node=auto --master-port=29507 st_trainer_ddp_hf.py \
+    --model_name "nasa-impact/indus-sde-st-v0.2" \
+    --num_train_epochs 1 \
+    --batch_size 64 \
+    --gradient_accumulation_steps 6 \
+    --lr 4e-5 \
+    --eval_and_save_steps 500 \
+    --output_base "../training_output" \
+    --max_datapoints_per_src_for_eval 1000 \
+    --warmup_ratio 0.02 \
+    --cosine_cycle_steps_custom 2000 
